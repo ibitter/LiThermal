eca62164314405b7539ec3494aab4a8e7005b0fb
ibitter <ibitter@qq.com>
2025-03-17 21:14:37 +0800

    Update batteryCard.cpp

--- src/ui/batteryCard.cpp
+++ src/ui/batteryCard.cpp
@@ -1,4 +1,5 @@
 #include <my_main.h>
+#include <cmath>
 #define BATTERY_CARD_X 250
 #define BATTERY_CARD_SHOW_Y -13
 #define BATTERY_CARD_HIDE_Y -43
@@ -60,13 +61,13 @@ void battery_card_check()
         {
             int16_t voltage = PowerManager_getBatteryVoltage();
               // 估算电量百分比
-            int16_t batteryPercentage = ((voltage - minVoltage) * 100) / (maxVoltage - minVoltage);
-            const char* percentage = batteryPercentage + "%";
-            if(batteryPercentage >= 100) percentage = "充电中";
+            int16_t batteryPercentage = (ceil((voltage - minVoltage) / 100) * 100) / (maxVoltage - minVoltage);
+            if(batteryPercentage < 0) batteryPercentage = 0;
+            if(batteryPercentage > 100) batteryPercentage = 100;
             if (voltage > 0)
             {
                 LOCKLV();
-                lv_label_set_text_fmt(lv_obj_get_child(card_Battery.obj, 0), "%s", percentage);
+                lv_label_set_text_fmt(lv_obj_get_child(card_Battery.obj, 0), "%d%%", batteryPercentage);
                 UNLOCKLV();
             }
             bool charging = PowerManager_isCharging();
